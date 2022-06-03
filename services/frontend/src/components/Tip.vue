46cdb48184b49b8900d06d52fa676dd4548b37ff
Marialuisabcs <maria.lbcs@icloud.com>
2022-06-03 03:08:13 -0400

    feat: implements component to insert usability tips

---
+++ services/frontend/src/components/Tip.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <div class="d-flex justify-center">
+      <v-alert shaped color="primary" class="terciary--text">
+        <div class="text-h6">Guia de utilização</div>
+        <div>
+          - Para ver o registro completo basta clicar no ícone
+          <v-icon class="mb-1" color="secondary"> mdi-plus-thick </v-icon>
+          <br />
+          - REGRAS DA BUSCA
+        </div>
+      </v-alert>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
