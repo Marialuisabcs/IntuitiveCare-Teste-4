4bce297854144ddd24ade9398789f73617c5d531
Marialuisabcs <maria.lbcs@icloud.com>
2022-06-03 03:08:46 -0400

    feat: inserts first idea of color palette

--- services/frontend/src/plugins/vuetify.js
+++ services/frontend/src/plugins/vuetify.js
@@ -3,4 +3,18 @@ import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+const vuetify = new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#3a3d70",
+        secondary: "#6c72d8",
+        terciary: "#e238c4",
+        background: "#b2b1bc", //b2b1bc
+        //anchor
+      },
+    },
+  },
+});
+
+export default vuetify;
