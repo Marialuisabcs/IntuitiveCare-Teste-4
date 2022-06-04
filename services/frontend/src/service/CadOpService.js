c2d3dfa9095b25ada6dcdc740ab11a08414e4c14
Marialuisabcs <maria.lbcs@icloud.com>
2022-06-03 21:41:41 -0400

    feat: fetch data from api

--- services/frontend/src/service/CadOpService.js
+++ services/frontend/src/service/CadOpService.js
@@ -1,6 +1,31 @@
 export default class CadOP {
+  async getOperadoras(page = 1, filter = null, query = null) {
+    if (filter != null) {
+      const res = await fetch(
+        "http://127.0.0.1:8000/?page=" +
+          page +
+          "&filterBy=" +
+          filter +
+          "&query=" +
+          query
+      ).catch((error) => {
+        return error;
+      });
+      const data = await res.json();
+      return data;
+    }
+    const res = await fetch("http://127.0.0.1:8000/?page=" + page).catch(
+      (error) => {
+        return error;
+      }
+    );
+    const data = await res.json();
+    return data;
+  }
+
   getHeaders() {
     const headers = [
+      { text: "id", value: "id" },
       { text: "Registro ANS", value: "registro_ans" },
       { text: "CNPJ", value: "cnpj" },
       { text: "Nome Fantasia", value: "nome_fantasia" },
