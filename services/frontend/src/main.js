5343ec20fbb4c85499501ae02ecfb14ccd7c3f98
Marialuisabcs <maria.lbcs@icloud.com>
2022-06-09 03:17:38 -0400

    removes axios (after deadline)

--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -2,10 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
-import axios from "axios";
-
-axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "http://localhost:5000/";
 
 Vue.config.productionTip = false;
 
