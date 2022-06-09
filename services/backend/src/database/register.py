c8833fbe405a4dfa21b3ace79a0ffce554aaefea
Marialuisabcs <maria.lbcs@icloud.com>
2022-06-09 00:14:14 -0400

    feat: removes obsolete lines and changes get_data method name (after deadline)

--- services/backend/src/database/register.py
+++ services/backend/src/database/register.py
@@ -1,7 +1,6 @@
 from database.database import db
 from database.models import RelacaoOperadoras
 from database.util import get_values
-import os
 
 
 def create_tables():
@@ -10,21 +9,20 @@ def create_tables():
 
 
 def insert_data():
-    data_count = get_data()
+    data_count = get_data_count()
+
     if data_count != 0:
         print(f"{data_count} rows already inserted")
         return
 
-    print(os.getcwd())
-
-    list_of_values = get_values("static/relatorio_cadop teste 4.csv")
+    list_of_values = get_values()
     field_names = list(RelacaoOperadoras._meta.fields.keys())[1:]
 
     with db.atomic():
         RelacaoOperadoras.insert_many(list_of_values, fields=field_names).execute()
 
 
-def get_data():
+def get_data_count():
     cursor = db.execute_sql("select count(*) from relacaooperadoras;")
     return cursor.fetchall()[0][0]
 
